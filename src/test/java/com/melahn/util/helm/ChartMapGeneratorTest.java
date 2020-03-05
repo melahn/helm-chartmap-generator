e384c9c82c483fffef4290927c350e28f9b1b1fd
melahn <gregorymelahn@gmail.com>
2020-03-05 08:49:23 -0500

    started to add tests, renamed class

---
+++ src/test/java/com/melahn/util/helm/ChartMapGeneratorTest.java
@@ -0,0 +1,28 @@
+package com.melahn.util.helm;
+
+import org.junit.Test;
+
+import java.io.*;
+
+import static org.junit.Assert.fail;
+
+/*
+ Tests the integrity of the jar by executing it to get the command line help
+ */
+public class ChartMapGeneratorTest {
+    @Test
+    public void testHelp() {
+        final String helpTextExpected = ChartMapGenerator.getHelp();
+        try {
+            Process process = Runtime.getRuntime().exec("java -jar helm-chartmap-generator-1.0-SNAPSHOT.jar", null, new File("./resource/jar"));
+            InputStream reader = process.getInputStream();
+            byte[] b = new byte[helpTextExpected.length()];
+            int i = 0;
+            while ((i < b.length) && ((reader.read(b, i++, 1)) != -1)) {};
+            String helpText = new String(b);
+            assert (helpText.equals(helpTextExpected));
+        } catch (IOException e) {
+            fail("testHelp failed:" + e.getMessage());
+        }
+    }
+}
