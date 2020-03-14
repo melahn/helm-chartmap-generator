eaf9daba5b93b374ac788dc717b7ff6bb265d587
melahn <gregorymelahn@gmail.com>
2020-03-14 17:22:42 -0400

    added code to generate an index.html file of the printed chart maps

--- src/main/java/com/melahn/util/helm/ChartMapGenerator.java
+++ src/main/java/com/melahn/util/helm/ChartMapGenerator.java
@@ -2,6 +2,9 @@ package com.melahn.util.helm;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.HashSet;
 import java.util.Map;
 
@@ -16,6 +19,7 @@ import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
+
 public class ChartMapGenerator {
 
     private static String localRepoName = null;
@@ -24,6 +28,7 @@ public class ChartMapGenerator {
     private static boolean verbose = false;
     private static String formatString = "";
     private static HashSet<String> extensions = new HashSet<String>();
+    private static String indexFilename = null;
 
     /**
      * Parses the command line and generates a set of reports
@@ -78,11 +83,16 @@ public class ChartMapGenerator {
             }
             if (cmd.hasOption("f")) {
                 formatString = cmd.getOptionValue("f");
-                log("format string".concat(formatString).concat("\" will be used"));
+                log("format string \"".concat(formatString).concat("\" will be used"));
             }
             if (cmd.hasOption("n")) {
                 count = Integer.parseInt(cmd.getOptionValue("n"));
-                log("A maximum of ".concat(cmd.getOptionValue("n")).concat(" versions will be printed"));
+                if (count == 1) {
+                    log("Only one version of each chart will be printed");
+                }
+                else {
+                    log("A maximum of ".concat(cmd.getOptionValue("n")).concat(" versions of each chart will be printed"));
+                }
             }
             if (cmd.hasOption("e")) {
                 envFilename = cmd.getOptionValue("e");
@@ -145,6 +155,7 @@ public class ChartMapGenerator {
             cache = mapper.readValue(new File(f), HelmChartLocalCache.class);
             Map<String, HelmChart[]> entries = cache.getEntries();
             log(String.valueOf(entries.size()).concat(" charts found"));
+            createIndex();
             for (Map.Entry<String, HelmChart[]> entry : entries.entrySet()) {
                 int i = 1;
                 for (HelmChart h : entry.getValue()) {
@@ -155,7 +166,8 @@ public class ChartMapGenerator {
                     }
                 }
             }
-        } catch (IOException e) {
+            endIndex();
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
@@ -190,9 +202,11 @@ public class ChartMapGenerator {
 
         try {
             log("printing chart: " + h.getNameFull());
+            startStanzaInIndex(h.getNameFull());
             for (String e : extensions) {
                 printChart(h, e);
             }
+            closeStanzaInIndex();
         } catch (Exception e) {
             System.out.println("Exception printing Chart " + h.getNameFull() + " : " + e.getMessage());
         }
@@ -216,10 +230,103 @@ public class ChartMapGenerator {
                     false,
                     false);
             testMap.print();
+            addChartToIndex(filename);
             log(filename.concat(filename.concat(" created")));
         } catch (Exception e) {
             System.out.println("Exception creating file ".concat(filename).concat(" : ").concat(e.getMessage()));
         }
+    }
+
+    /**
+     * Creates the index file and adds a head element
+     *
+     */
+    private static void createIndex() throws Exception {
+        indexFilename = outputDirname.concat("/index.html");
+        String h = "<!DOCTYPE HTML>\n<html>\n";
+        try {
+            Files.write(Paths.get(indexFilename),
+                    h.getBytes(),
+                    StandardOpenOption.CREATE_NEW,
+                    StandardOpenOption.WRITE);
+            addHead();
+        } catch (IOException e) {
+            throw (new Exception("Error creating index file ".concat(indexFilename).concat(".  Possibly it already exists")));
+        }
+    }
+
+    /**
+     * Adds head elements to the index file
+     *
+     */
+    private static void addHead () throws IOException {
+        String h = "<head>"
+                .concat("\t<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"/>\n")
+                .concat("\t<title>Helm Dependency Charts</title>\n");
+        Files.write(Paths.get(indexFilename),
+                h.getBytes(),
+                StandardOpenOption.APPEND);
+        addStyle();
+    }
+
+    /**
+     * Adds style elements to the index file, closes the head and starts the body
+     *
+     */
+    private static void addStyle () throws IOException {
+        String s = "\t<style>\n"
+                .concat("\t\t.header {").concat("\n\t\t\tfont: 30px sans-serif;\n\t\t}\n")
+                .concat("\t\t.chartname {").concat("\n\t\t\tfont-weight:bold;\n\t\t\tfont: 16px sans-serif;\n\t\t}\n")
+                .concat("\t\t.chartlink {").concat("\n\t\t\tfont: 16px sans-serif;\n\t\t}\n")
+                .concat("\t</style>\n</head>\n<body>\n\t<h1 class=\"header\">Helm Dependency Charts</h1>\n");
+        Files.write(Paths.get(indexFilename),
+                s.getBytes(),
+                StandardOpenOption.APPEND);
+    }
+
+    /**
+     * Starts one chart stanza with the chartname to the index file
+     *
+     */
+    private static void startStanzaInIndex(String s) throws IOException {
+        String l = "\n\t<p class=\"chartname\">".concat(s).concat("</p>\n\t<ul>\n");
+        Files.write(Paths.get(indexFilename),
+                l.getBytes(),
+                StandardOpenOption.APPEND);
+    }
+
+    /**
+     * Closes a stanza in the index file
+     *
+     */
+    private static void closeStanzaInIndex() throws IOException {
+        String l = "\t\t</ul>";
+        Files.write(Paths.get(indexFilename),
+                l.getBytes(),
+                StandardOpenOption.APPEND);
+    }
+
+    /**
+     * Adds a single chart filename to the index file
+     *
+     * @param f helm chart filename
+     */
+    private static void addChartToIndex(String f) throws IOException {
+        String l = "\t\t<li class=\"chartlink\">".concat("<a href=\"./").concat(f).concat("\">").concat(f).concat("</a>").concat("</li>\n");
+        Files.write(Paths.get(indexFilename),
+                l.getBytes(),
+                StandardOpenOption.APPEND);
+    }
+
+    /**
+     * Adds end elements to the index file
+     *
+     */
+    private static void endIndex () throws IOException {
+        String e = "</body></html>";
+        Files.write(Paths.get(indexFilename),
+                e.getBytes(),
+                StandardOpenOption.APPEND);
     }
 
     /**
